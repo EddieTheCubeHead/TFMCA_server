0475d5324aaeae989baf37de75fe5a85bc3168f6
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2020-05-03 11:26:52 +0300

    Eventtien tallentamisen perusta

--- src/main/java/com/example/TFMCA_server/DatabaseHandler.java
+++ src/main/java/com/example/TFMCA_server/DatabaseHandler.java
@@ -1,5 +1,10 @@
 package com.example.TFMCA_server;
 
+import com.example.TFMCA_server.gameEvents.CardCostPacket;
+import com.example.TFMCA_server.gameEvents.CardEventPacket;
+import com.example.TFMCA_server.gameEvents.ResourceEventPacket;
+import com.example.TFMCA_server.gameEvents.TileEventPacket;
+
 import java.sql.*;
 
 //https://www.tutorialspoint.com/jdbc/index.htm
@@ -91,17 +96,22 @@ public class DatabaseHandler {
         create_game.setInt(11, must_max_venus ? 1 : 0);
         create_game.setInt(12, turmoil_terraforming_revision ? 1 : 0);
         create_game.executeUpdate();
+        create_game.close();
         System.out.println("Game created");
     }
 
-    public static ResultSet getGame(String code) throws SQLException {
+    public static Integer getGameId(String code) throws SQLException {
         PreparedStatement get_game;
 
         String get_game_string = "SELECT * FROM tfmca.games WHERE code = ?";
 
         get_game = db_connection.prepareStatement(get_game_string);
         get_game.setString(1, code);
-        return get_game.executeQuery();
+        ResultSet rs = get_game.executeQuery();
+        if (!rs.next()) {
+            return null;
+        }
+        return rs.getInt("id");
     }
 
     public static void addPlayer(String user, String code, String player_position) throws SQLException {
@@ -114,5 +124,101 @@ public class DatabaseHandler {
         add_player.setString(2, code);
         add_player.executeUpdate();
         add_player.close();
+    }
+
+    public static void saveEvent(CardCostPacket packet, String game_code, Integer action_number, Integer generation) throws SQLException {
+        PreparedStatement save_event;
+
+        String save_string = "INSERT INTO tfmca.cardcostevents VALUES (parentGame, player, money, steel, titanium, heat, plantResource, floaterResource, actionNumber, generation) VALUES ((SELECT id FROM tfmca.games WHERE id=?), (SELECT userName FROM tfmca.users WHERE userName=?), ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Integer game_id = getGameId(game_code);
+        if (game_id == null) {
+            System.out.println("Error recording card cost event! Game id not found.");
+            return;
+        }
+
+        save_event = db_connection.prepareStatement(save_string);
+        save_event.setInt(1, game_id);
+        save_event.setString(2, packet.player_name);
+        save_event.setInt(3, packet.money);
+        save_event.setInt(4, packet.steel);
+        save_event.setInt(5, packet.titanium);
+        save_event.setInt(6, packet.heat);
+        save_event.setInt(7, packet.plant_resources);
+        save_event.setInt(8, packet.floater_resources);
+        save_event.setInt(9, action_number);
+        save_event.setInt(10, generation);
+
+        save_event.executeUpdate();
+        save_event.close();
+    }
+
+    public static void saveEvent(CardEventPacket packet, String game_code, Integer action_number, Integer generation) throws SQLException {
+        PreparedStatement save_event;
+
+        String save_string = "INSERT INTO tfmca.cardplayevents VALUES (parentGame, player, card, metadata, actionNumber, generation) VALUES ((SELECT id FROM tfmca.games WHERE id=?), (SELECT userName FROM tfmca.users WHERE userName=?), ?, ?, ?, ?)";
+
+        Integer game_id = getGameId(game_code);
+        if (game_id == null) {
+            System.out.println("Error recording card play event! Game id not found.");
+            return;
+        }
+
+        save_event = db_connection.prepareStatement(save_string);
+        save_event.setInt(1, game_id);
+        save_event.setString(2, packet.player_name);
+        save_event.setString(3, packet.card_name);
+        save_event.setInt(4, packet.metadata);
+        save_event.setInt(5, action_number);
+        save_event.setInt(6, generation);
+
+        save_event.executeUpdate();
+        save_event.close();
+    }
+
+    public static void saveEvent(ResourceEventPacket packet, String game_code, Integer action_number, Integer generation) throws SQLException {
+        PreparedStatement save_event;
+
+        String save_string = "INSERT INTO tfmca.cardplayevents VALUES (parentGame, card, changeAmount, actionNumber, generation) VALUES ((SELECT id FROM tfmca.games WHERE id=?), ?, ?, ?, ?)";
+
+        Integer game_id = getGameId(game_code);
+        if (game_id == null) {
+            System.out.println("Error recording resource event! Game id not found.");
+            return;
+        }
+
+        save_event = db_connection.prepareStatement(save_string);
+        save_event.setInt(1, game_id);
+        save_event.setString(2, packet.card_name);
+        save_event.setInt(3, packet.change);
+        save_event.setInt(4, action_number);
+        save_event.setInt(5, generation);
+
+        save_event.executeUpdate();
+        save_event.close();
+    }
+
+    public static void saveEvent(TileEventPacket packet, String game_code, Integer action_number, Integer generation) throws SQLException {
+        PreparedStatement save_event;
+
+        String save_string = "INSERT INTO tfmca.cardplayevents VALUES (parentGame, player, tileType, xCoord, yCoord, actionNumber, generation) VALUES ((SELECT id FROM tfmca.games WHERE id=?), (SELECT userName FROM tfmca.users WHERE userName=?), ?, ?, ?, ?, ?)";
+
+        Integer game_id = getGameId(game_code);
+        if (game_id == null) {
+            System.out.println("Error recording tile event! Game id not found.");
+            return;
+        }
+
+        save_event = db_connection.prepareStatement(save_string);
+        save_event.setInt(1, game_id);
+        save_event.setString(2, packet.player_name);
+        save_event.setString(3, packet.tile_type.name());
+        save_event.setInt(4, packet.x_coord);
+        save_event.setInt(5, packet.y_coord);
+        save_event.setInt(6, action_number);
+        save_event.setInt(7, generation);
+
+        save_event.executeUpdate();
+        save_event.close();
     }
 }
--- src/main/java/com/example/TFMCA_server/WebSocketHandler.java
+++ src/main/java/com/example/TFMCA_server/WebSocketHandler.java
@@ -2,18 +2,28 @@ package com.example.TFMCA_server;
 
 import com.example.TFMCA_server.errors.InvalidPasswordException;
 import com.example.TFMCA_server.errors.InvalidUsernameException;
+import com.example.TFMCA_server.gameEvents.CardCostPacket;
+import com.example.TFMCA_server.gameEvents.CardEventPacket;
+import com.example.TFMCA_server.gameEvents.ResourceEventPacket;
+import com.example.TFMCA_server.gameEvents.TileEventPacket;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.web.socket.PingMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.regex.Pattern;
 
 
 public class WebSocketHandler extends AbstractWebSocketHandler {
+    private static GsonBuilder builder = new GsonBuilder();
+    private static Gson gson = builder.create();
+
     static List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
     @Override
@@ -26,6 +36,8 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
         String game_code;
         String packet_type;
         String gson_packet;
+        Integer action_number;
+        Integer generation;
         System.out.println("WebSocket message from session " + session.getId());
 
         switch (identifier) {
@@ -86,10 +98,48 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
                 game_code = contents[3];
                 packet_type = contents[4];
                 gson_packet = contents[5];
+                action_number = Integer.parseInt(contents[6]);
+                generation = Integer.parseInt(contents[7]);
                 if (!SessionIdHandler.verify_session(user, session_id)) {
                     session.sendMessage(new TextMessage("session_exception;Invalid session"));
                 }
-                //TODO databaseen kirjaaminen
+                switch (packet_type) {
+                    case "card_event":
+                        CardEventPacket event_packet = gson.fromJson(gson_packet, CardEventPacket.class);
+                        try {
+                            DatabaseHandler.saveEvent(event_packet, game_code, action_number, generation);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "card_cost":
+                        CardCostPacket cost_packet = gson.fromJson(gson_packet, CardCostPacket.class);
+                        try {
+                            DatabaseHandler.saveEvent(cost_packet, game_code, action_number, generation);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "resource_event":
+                        ResourceEventPacket resource_packet = gson.fromJson(gson_packet, ResourceEventPacket.class);
+                        try {
+                            DatabaseHandler.saveEvent(resource_packet, game_code, action_number, generation);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "tile_event":
+                        TileEventPacket tile_packet = gson.fromJson(gson_packet, TileEventPacket.class);
+                        try {
+                            DatabaseHandler.saveEvent(tile_packet, game_code, action_number, generation);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    default:
+                        break;
+
+                }
                 GameSessionHandler.sendMessage(game_code, session, String.format("game_action;%s;%s", packet_type, gson_packet));
                 break;
 
